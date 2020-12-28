1c0bc681ee82a6f8c2b89b367648f58ca15ded15
hskim <andykim881028@gmail.com>
2020-12-28 18:31:51 +0900

    Learn Prop and Add propTypes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-function Food(props) {
-    console.log();
-    let food = props.fav;
-    return <h1>I like {food}</h1>
+function Food({id, name, picture}) {
+    return <div>
+        <h1>ID : {id}</h1>
+        <h1>I like {name}</h1>
+        <img src={picture} alt={name}/>
+    </div>
+
 }
 
+const foodILike = [
+    {
+        id: 1,
+        name: "banana2",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.XFvgjrUne5hvG2AZFEjjzgHaEK%26pid%3DApi&f=1",
+        rating: 4.9
+    },
+    {
+        id: 2,
+        name: "apple2",
+        image: "https://ko.m.wikipedia.org/wiki/%ED%8C%8C%EC%9D%BC:Logo_Apple.inc.gif",
+        rating: 5.0
+    },
+]
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-        <Food
-            fav="banana"
-            something={"true"}
-            test={["hello", 1, 2, 3, false]}
-        />
-        <Food
-            fav="apple"
-            something={"true"}
-            test={["hello", 1, 2, 3, false]}
-        />
-        <Food
-            fav="ramen"
-            something={"true"}
-            test={["hello", 1, 2, 3, false]}
-        />
-    </div>
-  );
+    return (
+        <div className="App">
+            {
+                foodILike.map(item => (
+                    <Food
+                        id={item.id}
+                        name={item.name}
+                        picture={item.image}/>
+                ))}
+        </div>
+    );
 }
 
 export default App;
