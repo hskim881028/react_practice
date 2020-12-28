0dc53eb2e0dc6194a156e6138dbe61c2daf84140
hskim <andykim881028@gmail.com>
2020-12-28 18:05:03 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import Patato from "./Potato"
+function Food(props) {
+    console.log();
+    let food = props.fav;
+    return <h1>I like {food}</h1>
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-        <Patato />
+        <Food
+            fav="banana"
+            something={"true"}
+            test={["hello", 1, 2, 3, false]}
+        />
+        <Food
+            fav="apple"
+            something={"true"}
+            test={["hello", 1, 2, 3, false]}
+        />
+        <Food
+            fav="ramen"
+            something={"true"}
+            test={["hello", 1, 2, 3, false]}
+        />
     </div>
   );
 }
